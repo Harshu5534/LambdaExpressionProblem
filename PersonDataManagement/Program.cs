d2e56cb00648b0c79248c87d8967e7f448247558
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-05-20 15:15:44 +0530

    [Harshal]Add:Get Records Between 13 to 18 In List

--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -9,7 +9,7 @@ namespace PersonDataManagement
             Console.WriteLine("-------Welcome To Person Data Management Program------");
             PersonManagement management = new PersonManagement();
             bool end = true;
-            Console.WriteLine("1.GetTopTwoRecords\n2.GetRecordBetween13to18\n3.End Of The Program");
+            Console.WriteLine("1.GetTopTwoRecords\n2.GetRecordsBetween13to18\n3.End Of The Program");
             while (end)
             {
                 Console.WriteLine("\nEnter Option For Exicute The Program");
